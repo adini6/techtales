49cd86ffac6a187591dc7abe96f79f7d3314e0f7
Abdikalik Dini <adini18@gmail.com>
2023-10-11 16:32:55 -0400

    created routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
+const userRoutes = require('./routes/userroute');
+const blogRoutes = require('./routes/blogroute');
+require('dotenv').config();
+
 
 const sequelize = require('./config/connection');
 const { User, BlogPost, Comment } = require('./models');
@@ -14,6 +18,10 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+//routes
+app.use('/api/users', userRoutes);
+app.use('/api/blogs', blogRoutes);
+
 //handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -21,10 +29,10 @@ app.set('view engine', 'handlebars');
 
 //session setup
 const sess =  {
-    secret: 'primetime',
+    secret: process.env.SESSION_SECRET || 'development_secret',
     cookie: {},
     resave: false,
-    saveUnintialized: true,
+    saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
