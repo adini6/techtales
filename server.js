18d63d54ac9b6d42ed79292c4b21fababd7cc876
Abdikalik Dini <adini18@gmail.com>
2023-10-11 16:14:09 -0400

    added to server.js

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+const { User, BlogPost, Comment } = require('./models');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-app.use(express.urlencoded({ extended: true }));
+//middlewares
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
-const exphbs = require('express-handlebars');
-
+//handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
 
+//session setup
+const sess =  {
+    secret: 'primetime',
+    cookie: {},
+    resave: false,
+    saveUnintialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+});
