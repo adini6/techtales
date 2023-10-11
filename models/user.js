cd8a3b0f2a3d63a841b5e0fbe42b7f3b54d2bcbf
Abdikalik Dini <adini18@gmail.com>
2023-10-11 00:30:50 -0400

    finished user model

--- models/user.js
+++ models/user.js
@@ -2,4 +2,56 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config.js');
 const bcrypt = require('bcrypt');
 
-class
+//check password
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        //ensuress password is hashed before saving it 
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.pass, 10);
+                return newUserData;
+            },
+            //password is hased if it's updated
+            beforeUpdate: async (updatedUserData) => {
+                if(updatedUserData.password){
+                    updatedUserData.password = await bycrpt.hash(updatedUserData.password, 10);
+                }
+                return updatedUserData;
+            }
+
+        },
+        sequelize,
+        timestamps:false,
+        freezeTableName: true,
+        underscored:true,
+        modelName: 'user'
+    }
+
+);
+module.exports = User;
