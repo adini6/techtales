11f9a52aa877f8a833c48741767e4dd474ef80f3
Abdikalik Dini <adini18@gmail.com>
2023-10-10 23:44:51 -0400

    updated from config.json to config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
   },
   "test": {
     "username": "root",
